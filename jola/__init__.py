a81156399d6a78961843b2c97d081c2c502582ce
LidaDavydova <davydoval2005@gmail.com>
2026-02-24 11:45:04 +0300

    test qwen

--- jola/__init__.py
+++ jola/__init__.py
@@ -1,5 +1,5 @@
-from .modeling_llama import JoLAModel_llama
-from .modeling_qwen import JoLAModel_qwen
+from .modeling_llama import JoLAModel as JoLAModel_llama
+from .modeling_qwen import JoLAModel as JoLAModel_qwen
 from .trainers import JoLATrainer, make_data_collator
 from .config import JoLAConfig
 from .dataset import JoLADataset
